257c04dc0f111e03780f7e285c96bb2a8cfea274
Hugo Jeller <hugo_jeller@hotmail.com>
2020-04-06 03:00:02 +0000

    Updated checkout layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,59 +5,6 @@
 @section('content')
     @include('cards.servers')
     <div class="flex justify-center">
-        <div class="w-1/3 p-8">
-            <div class="mt-8">
-                <div class="px-6 py-4 flex justify-between bg-red-600 text-white text-lg rounded-t">
-                    <div class="">vCore</div>
-                    <div>
-                        <span class="font-medium">R$ 50,00</span>
-                        <small class="text-xs text-red-200">/100% vCore</small>
-                    </div>
-                </div>
-                <div class="flex p-4 border border-t-0 rounded-b">
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/4 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">25%</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/4 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">50%</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/4 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">75%</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/4 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">100%</div>
-                </div>
-            </div>
-            <div class="mt-8">
-                <div class="px-6 py-4 flex justify-between bg-blue-600 text-white text-lg rounded-t">
-                    <div class="">RAM</div>
-                    <div>
-                        <span class="font-medium">R$ 20,00</span>
-                        <small class="text-xs text-blue-200">/GB</small>
-                    </div>
-                </div>
-                <div class="flex p-4 border border-t-0 rounded-b">
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/6 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">1GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/6 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">2GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/6 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">3GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/6 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">4GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/6 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">5GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/6 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">6GB</div>
-                </div>
-            </div>
-            <div class="mt-8">
-                <div class="px-6 py-4 flex justify-between bg-green-600 text-white text-lg rounded-t">
-                    <div class="">Storage</div>
-                    <div>
-                        <span class="font-medium">R$ 5,00</span>
-                        <small class="text-xs text-green-200">/10 GB</small>
-                    </div>
-                </div>
-                <div class="flex p-4 border border-t-0 rounded-b">
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/5 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">10GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/5 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">20GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/5 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">30GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/5 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">40GB</div>
-                    <div class="trans flex items-center justify-center py-4 mx-2 w-1/5 bg-gray-100 text-2xl border border-gray-400 cursor-pointer rounded hover:shadow-md">50GB</div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="flex justify-center">
         <div class="p-4 w-2/3">
             <div class="mt-8">
                 <div class="flex items-stretch bg-green-600 overflow-hidden rounded-t-lg">
@@ -66,10 +13,10 @@
                     </div>
                     <div class="flex flex-grow-0 flex-column items-center px-8 py-3 text-3xl text-white ">
                         <div>
-                            <small class="font-light text-xl text-green-200">R$ </small>
+                            <small class="font-light text-xl text-green-100">R$ </small>
                             <span class="font-medium">50</span>
                         </div>
-                        <div class="font-normal text-green-200 tracking-tight text-xs">por 100% CPU</div>
+                        <div class="font-normal text-green-200 tracking-tight text-sm">por 100% CPU</div>
                     </div>
                 </div>
                 <div class="flex px-2 py-4 bg-gray-100 border border-t-0 rounded-b-lg">
@@ -96,10 +43,10 @@
                     </div>
                     <div class="flex flex-grow-0 flex-column items-center px-8 py-3 text-3xl text-white ">
                         <div>
-                            <small class="font-light text-xl text-red-200">R$ </small>
+                            <small class="font-light text-xl text-red-100">R$ </small>
                             <span class="font-medium">20</span>
                         </div>
-                        <div class="font-normal text-red-200 tracking-tight text-xs">por 1GB</div>
+                        <div class="font-normal text-red-200 tracking-tight text-sm">por 1GB</div>
                     </div>
                 </div>
                 <div class="flex px-2 py-4 bg-gray-100 border border-t-0 rounded-b-lg">
@@ -130,10 +77,10 @@
                     </div>
                     <div class="flex flex-grow-0 flex-column items-center px-8 py-3 text-3xl text-white ">
                         <div>
-                            <small class="font-light text-xl text-blue-200">R$ </small>
+                            <small class="font-light text-xl text-blue-100">R$ </small>
                             <span class="font-medium">5</span>
                         </div>
-                        <div class="font-normal text-blue-200 tracking-tight text-xs">por 10GB</div>
+                        <div class="font-normal text-blue-200 tracking-tight text-sm">por 10GB</div>
                     </div>
                 </div>
                 <div class="flex px-2 py-4 bg-gray-100 border border-t-0 rounded-b-lg">
@@ -159,24 +106,50 @@
             <div class="mt-8">
                 <div class="bg-green-600 rounded-t-lg rounded-b-lg">
                     <div class="flex items-center px-6 py-4 mb-1 text-3xl text-gray-100 bg-gray-900 rounded-t-lg shadow-lg">
-                        Total
+                        Resumo
                     </div>
                     <div class="p-4 bg-gray-100 border border-t-0 rounded-b-lg">
                         <div class="flex flex-col text-xl">
                             <div class="flex justify-between">
-                                <span class="">50% CPU</span>
-                                <span class="">R$ 25,00</span>
+                                <p class="font-bold text-gray-700">
+                                    CPU:
+                                    <span class="ml-1 font-normal text-lg">50%</span>
+                                </p>
+                                <p class="">R$ 25,00</p>
                             </div>
                             <div class="flex justify-between">
-                                <span class="">2GB RAM</span>
-                                <span class="">R$ 40,00</span>
+                                <p class="font-bold text-gray-700">
+                                    RAM:
+                                    <span class="ml-1 font-normal text-lg">2GB</span>
+                                </p>
+                                <p class="">R$ 40,00</p>
                             </div>
                             <div class="flex justify-between">
-                                <span class="">10GB SSD</span>
-                                <span class="">R$ 5,00</span>
+                                <p class="font-bold text-gray-700">
+                                    SSD:
+                                    <span class="ml-1 font-normal text-lg">10GB</span>
+                                </p>
+                                <p class="">R$ 5,00</p>
                             </div>
+                            <div class="flex justify-between">
+                                <p class="font-bold text-gray-700">
+                                    Databases:
+                                    <span class="ml-1 font-normal text-lg">1</span>
+                                </p>
+                                <p class="">R$ 5,00</p>
+                            </div>
+
+                            <hr class="my-2">
+
+                            <div class="flex justify-between text-2xl">
+                                <p class="font-bold text-gray-700">
+                                    TOTAL
+                                </p>
+                                <p class="font-bold">R$ 75,00</p>
+                            </div>
+
                         </div>
-                        <div class="mt-8 px-6 py-4 bg-green-600 font-normal text-center text-gray-100 text-2xl tracking-wide rounded-lg shadow-md">
+                        <div class="mt-4 px-6 py-4 bg-green-600 font-normal text-center text-gray-100 text-2xl tracking-wide rounded-lg shadow-md">
                             Finalizar
                         </div>
                     </div>
