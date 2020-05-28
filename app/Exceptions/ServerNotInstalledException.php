f5c37bd184474d5a2b9d0e513a15b0dacdab4efd
Hugo Jeller <hugo_jeller@hotmail.com>
2020-05-28 09:09:07 +0000

    Turn ServerNotInstalledException into FlashException

--- app/Exceptions/ServerNotInstalledException.php
+++ app/Exceptions/ServerNotInstalledException.php
@@ -4,7 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 
-class ServerNotInstalledException extends Exception
+class ServerNotInstalledException extends Exception implements FlashException
 {
-    //
+    public function __construct()
+    {
+        parent::__construct('This server is not installed yet!');
+    }
 }
