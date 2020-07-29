d8cbdd96bc5590b3f137ab1d70d20cde67e37aa5
Hugo Jeller <hugo_jeller@hotmail.com>
2020-07-29 08:49:13 +0000

    Added User locale and Accept-Language headers middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Orkhanahmadov\LaravelAcceptLanguageMiddleware\Middleware::class,
+        \App\Http\Middleware\SetLocale::class,
     ];
 
     /**
---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!auth()->check() || ($locale = auth()->user()->locale)) {
+            app()->setLocale($locale);
+
+            return redirect('home');
+        }
+
+        return $next($request);
+    }
+}
