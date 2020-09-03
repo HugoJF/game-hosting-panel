28a650c0d5b2c9c5a3f4f89d5116bbe4eeba6f97
Hugo Jeller <hugo_jeller@hotmail.com>
2020-09-03 13:31:03 +0000

    Only add GA tag on production

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -21,15 +21,18 @@
 @yield('content')
 
 @stack('scripts')
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-78711465-7"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
 
-    gtag('config', 'UA-78711465-7');
-</script>
+@if(app()->environment('production'))
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-78711465-7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-78711465-7');
+    </script>
+@endif
 
 </body>
 </html>
