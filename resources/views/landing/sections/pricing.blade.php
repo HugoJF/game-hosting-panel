9d7b8b6dfd77e2795aae55ce3e5f4301274088b8
Hugo Jeller <hugo_jeller@hotmail.com>
2020-09-12 18:09:08 +0000

    Added game name to game-card

--- resources/views/landing/sections/pricing.blade.php
+++ resources/views/landing/sections/pricing.blade.php
@@ -1,28 +1,31 @@
 <x-section
-    title="Servidores para qualquer hora"
-    description="Crie seu próprio servidor em minutos e pague apenas pelo tempo online!"
+    title="Hospedagem de servidores para qualquer hora"
+    description="Hospede seu próprio servidor em minutos e pague apenas pelo tempo online!"
     theme="blue"
 >
     <div class="px-8 grid grid-cols-2 md:grid-cols-3 gap-8 items">
         @include('landing.components.game-card', [
+            'game' => 'Minecraft Vanilla',
             'img' => asset("images/minecraft.png"),
-            'alt' => 'Minecraft | Paper Server',
+            'alt' => 'Hospedagem de servidor de Minecraft Vanilla',
             'specs' => '~10 slots',
             'description' => '1200 marks + 1GB RAM + 5GB SSD',
             'price' => 0.73,
             'period' => 'por hora'
         ])
         @include('landing.components.game-card', [
+	        'game' => 'Counter-Strike: Global Offensive',
             'img' => asset("images/csgo.png"),
-            'alt' => 'Counter Strike: Global Offensive',
+            'alt' => 'Hospedagem de servidor de Counter Strike: Global Offensive',
             'specs' => '10 slots de 128 tick',
             'description' => '1800 marks + 1GB RAM + 30GB SSD',
             'price' => 1.11,
             'period' => 'por hora'
         ])
         @include('landing.components.game-card', [
+            'game' => 'Call of Duty 4: Modern Warfare',
             'img' => asset("images/cod4.png"),
-            'alt' => 'Call of Duty 4 Xtended',
+            'alt' => 'Hospedagem de servidor de Call of Duty 4 Xtended',
             'specs' => '12 slots',
             'description' => '1800 marks + 1GB RAM + 20GB SSD',
             'price' => 0.34,
