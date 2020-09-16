e885ba98ee8a0429893f7f07230e67900b8378ad
Hugo Jeller <hugo_jeller@hotmail.com>
2020-09-16 03:16:52 +0000

    Remove debugging event from controller

--- app/Http/Controllers/User/ServerController.php
+++ app/Http/Controllers/User/ServerController.php
@@ -60,7 +60,6 @@ class ServerController extends Controller
 
     public function show(DeployCostService $costService, Server $server)
     {
-        event(new ServerInstalled($server));
         $last5 = $server->deploys()->latest()->orderBy('created_at', 'DESC')->limit(5)->get();
         $deploys = collect($last5->first() ? [$last5->first()] : []);
         $transactions = Transaction::findMany($last5->pluck('transaction_id'));
