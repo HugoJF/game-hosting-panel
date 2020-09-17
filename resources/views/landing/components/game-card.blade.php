34a3e670c83584d35beaa7547728882abf83b8d3
Hugo Jeller <hugo_jeller@hotmail.com>
2020-09-17 03:50:11 +0000

    Added daily price to landing page

--- resources/views/landing/components/game-card.blade.php
+++ resources/views/landing/components/game-card.blade.php
@@ -10,11 +10,19 @@
             {{ $specs }}
         </div>
 
-        <!-- Price -->
-        <div class="text-2xl lg:text-3xl text-blue-dark font-semibold">
-            R$ {{ number_format((float) $price, 2, ',', '.') }}
+        <!-- Hourly price -->
+        <div class="text-xl lg:text-2xl text-blue-dark font-semibold">
+            R$ {{ number_format((float) $hourlyPrice, 2, ',', '.') }}
             <small class="block md:inline leading-none md:leading-normal text-center text-base xl:text-lg text-gray-primary font-normal tracking-tighter">
-                {{ $period }}
+                por hora
+            </small>
+        </div>
+
+        <!-- Daily price -->
+        <div class="text-2xl lg:text-3xl text-blue-dark font-semibold">
+            R$ {{ number_format((float) $dailyPrice, 2, ',', '.') }}
+            <small class="block md:inline leading-none md:leading-normal text-center text-base xl:text-lg text-gray-primary font-normal tracking-tighter">
+                por dia
             </small>
         </div>
     </div>
--- resources/views/landing/sections/pricing.blade.php
+++ resources/views/landing/sections/pricing.blade.php
@@ -5,28 +5,28 @@
 >
     <div class="px-8 grid grid-cols-2 md:grid-cols-3 gap-8 items">
         @include('landing.components.game-card', [
-            'img' => asset("images/minecraft.png"),
-            'alt' => 'Minecraft | Paper Server',
-            'specs' => '~10 slots',
-            'description' => '1200 marks + 1GB RAM + 5GB SSD',
-            'price' => 0.73,
-            'period' => 'por hora'
+            'img'           => asset("images/minecraft.png"),
+            'alt'           => 'Minecraft | Paper Server',
+            'specs'         => '~10 slots',
+            'description'   => '1200 marks + 1GB RAM + 5GB SSD',
+            'hourlyPrice'   => 0.73,
+            'dailyPrice'    => 7.00,
         ])
         @include('landing.components.game-card', [
-            'img' => asset("images/csgo.png"),
-            'alt' => 'Counter Strike: Global Offensive',
-            'specs' => '10 slots de 128 tick',
-            'description' => '1800 marks + 1GB RAM + 30GB SSD',
-            'price' => 1.11,
-            'period' => 'por hora'
+            'img'           => asset("images/csgo.png"),
+            'alt'           => 'Counter Strike: Global Offensive',
+            'specs'         => '10 slots de 128 tick',
+            'description'   => '1800 marks + 1GB RAM + 30GB SSD',
+            'hourlyPrice'   => 0.99,
+            'dailyPrice'    => 9.47,
         ])
         @include('landing.components.game-card', [
-            'img' => asset("images/cod4.png"),
-            'alt' => 'Call of Duty 4 Xtended',
-            'specs' => '12 slots',
-            'description' => '1800 marks + 1GB RAM + 20GB SSD',
-            'price' => 0.34,
-            'period' => 'por hora'
+            'img'           => asset("images/cod4.png"),
+            'alt'           => 'Call of Duty 4 Xtended',
+            'specs'         => '12 slots',
+            'description'   => '1800 marks + 1GB RAM + 20GB SSD',
+            'hourlyPrice'   => 0.34,
+            'dailyPrice'    => 3.27,
         ])
     </div>
 
