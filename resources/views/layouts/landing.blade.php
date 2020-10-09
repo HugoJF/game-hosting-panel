f491f21ef7e01ab46d0972dba3a389c324c14f9e
Hugo Jeller <hugo_jeller@hotmail.com>
2020-10-09 17:48:44 -0400

    Landing page tweaks

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -4,7 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Host de_nerdTV</title>
+    <title>Hospedagem de servidores de jogos - de_nerdTV</title>
+
+    <meta name="description" content="Hospedagem de CS:GO, Minecraft e CoD4 no Brasil com proteção DDoS">
+    <meta property="og:description" content="Hospedagem de CS:GO, Minecraft e CoD4 no Brasil com proteção DDoS"/>
+    <meta property="og:title" content="Hospedagem de servidores de jogos - de_nerdTV"/>
+    <meta property="og:url" content="https://host.denerdtv.com/"/>
+    <meta property="og:type" content="website"/>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&subset=latin" rel="stylesheet">
