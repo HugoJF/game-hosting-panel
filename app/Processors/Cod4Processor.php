6f29af75a8936f94e75027f81160bc66e5456353
Hugo Jeller <hugo_jeller@hotmail.com>
2020-10-10 01:56:34 -0400

    Added CoD4 startup command

--- app/Processors/Cod4Processor.php
+++ app/Processors/Cod4Processor.php
@@ -22,7 +22,22 @@ class Cod4Processor extends Processor
      */
     public function formToStartupConfig(array $form): ?string
     {
-        return null;
+        $slots = $form['slots'];
+
+        $parts = [
+            './cod4x18_dedrun',
+            '+map {{COD_MAP}}',
+            '+set dedicated 2',
+            '+exec {{COD_CFG}}',
+            '+map_rotate',
+            '+set net_ip {{SERVER_IP}}',
+            '+set net_port {{SERVER_PORT}}',
+            '+set sv_maxclients {{COD_MAXPLAYERS}}',
+            "+set sv_maxclients $slots",
+            '+set sv_authtoken {{COD_AUTH}}'
+        ];
+
+        return implode(' ', $parts);
     }
 
     public function reject(array $resourceCost): bool
